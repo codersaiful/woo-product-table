d7d5f37c3b076e60e93a72c0609df7ab1a961568
Saiful Islam <codersaiful@gmail.com>
2020-07-11 12:54:26 +0600

    Version updated to 2.7.0.0

--- woo-product-table.php
+++ woo-product-table.php
@@ -7,7 +7,7 @@
  * Author URI: https://codecanyon.net/user/codeastrology
  * Tags: woocommerce product,woocommerce product table, product table
  * 
- * Version: 6.0.19
+ * Version: 2.7.0.0
  * Requires at least:    4.0.0
  * Tested up to:         5.3.2
  * WC requires at least: 3.0.0
