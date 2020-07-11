c35ff599ed9b25d2a4248772fe0773a2d346e790
Saiful Islam <codersaiful@gmail.com>
2020-07-11 16:44:09 +0600

    responsive issue of admin panel has been fixed

--- admin/functions.php
+++ admin/functions.php
@@ -31,15 +31,15 @@ if( !function_exists( 'wpt_admin_form_top' ) ){
         $validation = apply_filters( 'wpt_admin_form_top_links', true, $post );
         if( $validation ){
         ?>
-        <h3 class="wpt_admin_form_links" style="margin: 0">
-            <a class="wpt_get_pro_form_top_link" target="_blank" title="Awesome Pro features is Waiting for you!" href="https://codecanyon.net/item/woo-product-table-pro/20676867">Get Pro</a>  
-            <a target="_blank" href="https://www.wcproducttable.xyz/demo-list/">Demo</a>
-            <a target="_blank" href="https://www.wcproducttable.xyz/docs/">Basic Helps</a>
-            <a target="_blank" href="https://doc.codeastrology.com/woo-product-table-pro/#intro">Documentation</a>
-            <a target="_blank" href="https://codeastrology.com/support/">Get Support</a> 
-            <a target="_blank" href="https://codeastrology.com/support/forums/forum/woo-product-table-pro-making-quick-order-table/">Forum</a>
+        <ul class="wpt_admin_form_links" style="margin: 0">
+            <li><a class="wpt_get_pro_form_top_link" target="_blank" title="Awesome Pro features is Waiting for you!" href="https://codecanyon.net/item/woo-product-table-pro/20676867">Get Pro</a></li>  
+            <li><a target="_blank" href="https://www.wcproducttable.xyz/demo-list/">Demo</a></li>
+            <li><a target="_blank" href="https://www.wcproducttable.xyz/docs/">Basic Helps</a></li>
+            <li><a target="_blank" href="https://doc.codeastrology.com/woo-product-table-pro/#intro">Documentation</a></li>
+            <li><a target="_blank" href="https://codeastrology.com/support/">Get Support</a></li> 
+            <li><a target="_blank" href="https://codeastrology.com/support/forums/forum/woo-product-table-pro-making-quick-order-table/">Forum</a></li>
 
-        </h3>    
+        </ul>    
         <?php
         }
     }
