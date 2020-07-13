ca6bf93073b1dd0a248ff80705109589f54ccd95
Saiful Islam <codersaiful@gmail.com>
2020-07-13 17:26:36 +0600

    working for acf

--- admin/tabs/column_settings.php
+++ admin/tabs/column_settings.php
@@ -1,23 +1,34 @@
 <?php
 $colums_disable_array = WPT_Product_Table::$colums_disable_array; //For first time only
 
-$for_add =  $meta_column_array = $columns_array = $updated_columns_array = get_post_meta( $post->ID, 'column_array', true );
+$columns_array = WPT_Product_Table::$columns_array;
+//var_dump(WPT_Product_Table::$columns_array);
+$for_add =  $meta_column_array = $updated_columns_array = get_post_meta( $post->ID, 'column_array', true );
+
 if( !$meta_column_array && empty( $meta_column_array ) ){
     $for_add = $updated_columns_array = WPT_Product_Table::$columns_array;
 }
-$columns_array = WPT_Product_Table::$columns_array;
+if( $updated_columns_array && !empty( $updated_columns_array ) && !empty( $columns_array ) ){
+    $columns_array = array_merge( $columns_array, $updated_columns_array );
+}
+
+//var_dump(array_merge( $columns_array,$updated_columns_array ));
 //unset($columns_array['description']); //Again Start Description Column From V6.0.25
 $meta_enable_column_array = get_post_meta( $post->ID, 'enabled_column_array', true );
+if( $meta_enable_column_array && !empty( $meta_enable_column_array ) && !empty( $columns_array ) ){
+    $columns_array = array_merge($meta_enable_column_array,$columns_array);
+}
+
 $column_settings = get_post_meta( $post->ID, 'column_settings', true ); 
 //var_dump($column_settings);
 
 $additional_collumn = array_diff(array_keys($for_add), array_keys( WPT_Product_Table::$columns_array ));
 
-
+//var_dump($meta_enable_column_array,array_merge($meta_enable_column_array,$columns_array));
 
 //var_dump( $meta_enable_column_array, $columns_array );
 if( is_array( $meta_enable_column_array ) && !empty( $meta_enable_column_array ) ){
-    $columns_array = array_merge( $meta_enable_column_array, array_diff( $columns_array, $meta_enable_column_array ));
+    //$columns_array = array_merge( $meta_enable_column_array, array_diff( $columns_array, $meta_enable_column_array ));
 }
 //$columns_array = array_merge($meta_enable_column_array,array_diff($columns_array,$meta_enable_column_array));
 //var_dump($merge_column_array);
