d5c70717830f1543526102154fd89d8c80f08374
Saiful Islam <codersaiful@gmail.com>
2020-07-18 10:58:37 +0600

    stock old style change and added wc_get_stock_html() for stock

--- includes/items/stock.php
+++ includes/items/stock.php
@@ -1,9 +1,12 @@
 <?php
+echo wc_get_stock_html($product);
+/*
 $stock_status_message = $stock_status_message = $config_value['table_out_of_stock'];
 if( $data['stock_status'] == 'instock' ){
     $stock_status_message =  $data['stock_quantity'] . ' ' . $config_value['table_in_stock']; 
 }elseif( $data['stock_status'] == 'onbackorder' ){
     $stock_status_message = $config_value['table_on_back_order'];//'On Back Order';
 }
+ * //stock old style change and added wc_get_sthock
 echo "<span class='{$data['stock_status']}'>" . $stock_status_message . " </span>";
-
+*/
