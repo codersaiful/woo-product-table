b2f0f1e34a705bbdc103d4ac2ec7b39756d11cc0
Saiful Islam <codersaiful@gmail.com>
2020-09-17 13:55:59 +0600

    default file updated

--- includes/items/default.php
+++ includes/items/default.php
@@ -1,28 +1,5 @@
-<?php 
-$settings = isset( $column_settings[$keyword] ) ? $column_settings[$keyword] : false;
-var_dump($keyword, $settings);
-
-
-if( isset( $_GET['var_dump'] ) && !empty( $_GET['var_dump'] ) ){
-    $var_dump = $_GET['var_dump'];
-    $otput = isset( $$var_dump ) ? $$var_dump : 'undefined';
-    var_dump($otput);
-}
-if( $in_extra_manager ){
-    $wrap_info = 'Items inside TD';
-}else{
-    $wrap_info = 'Cell of TD';
-}
-
-?>
-<code><?php echo $wrap_info; ?>::<?php echo $keyword; ?></code>
-<pre>
-File Not Founded.
-Looking file at <?php echo $requested_file; ?><br>
-For More Details: your_page_url.com?debug=wpt
-</pre>
 <?php
-if( isset( $_GET['debug'] ) && !empty( $_GET['debug'] ) ){
+if( isset( $_GET['debug'] ) ){
 ?>
 <b>Filter for Locate File:</b>
 <pre>
@@ -36,3 +13,31 @@ add_filter('wpto_template_loc_item_<?php echo $keyword; ?>','your_prefix_your_fu
 <?php
 }
 ?>
+
+<?php 
+
+if( isset( $_GET['var_dump'] ) && !empty( $_GET['var_dump'] ) ){
+    $var_dump = $_GET['var_dump'];
+    $otput = isset( $$var_dump ) ? $$var_dump : 'undefined';
+    var_dump($otput);
+}if( isset( $_GET['var_dump'] ) && empty( $_GET['var_dump'] ) ){
+
+}else{
+    return;
+}
+$settings = isset( $column_settings[$keyword] ) ? $column_settings[$keyword] : false;
+var_dump($keyword, $settings);
+
+if( $in_extra_manager ){
+    $wrap_info = 'Items inside TD';
+}else{
+    $wrap_info = 'Cell of TD';
+}
+
+?>
+<code><?php echo $wrap_info; ?>::<?php echo $keyword; ?></code>
+<pre>
+File Not Founded.
+Looking file at <?php echo $requested_file; ?><br>
+For More Details: your_page_url.com?debug=wpt
+</pre>
