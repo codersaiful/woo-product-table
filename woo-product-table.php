f83bc64a17aa8230570430d2fadf3d9ccff410ba
Saiful Islam <codersaiful@gmail.com>
2020-12-13 10:10:26 +0600

    varsion updated to latest

--- woo-product-table.php
+++ woo-product-table.php
@@ -11,7 +11,7 @@
  * Requires at least:    4.0.0
  * Tested up to:         5.6
  * WC requires at least: 3.0.0
- * WC tested up to: 	 4.7.0
+ * WC tested up to: 	 4.8.0
  * 
  * Text Domain: wpt_pro
  * Domain Path: /languages/
