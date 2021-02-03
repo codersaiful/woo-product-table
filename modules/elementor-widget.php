65bb46db69adde6f442c36fbeb212589b1df7733
Saiful Islam <codersaiful@gmail.com>
2021-02-03 22:26:30 +0600

    Control for color and Typography for table is added

--- modules/elementor-widget.php
+++ modules/elementor-widget.php
@@ -1,4 +1,15 @@
 <?php
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+use Elementor\Scheme_Color;
+use Elementor\Group_Control_Typography;
+use Elementor\Scheme_Typography;
+use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Box_Shadow;
+use Elementor\Group_Control_Background;
+use Elementor\Repeater;
+
 
 class WPT_Elementor_Widget extends \Elementor\Widget_Base{
     
@@ -68,83 +79,16 @@ class WPT_Elementor_Widget extends \Elementor\Widget_Base{
 	 */
 	protected function _register_controls() {
 
-		$this->start_controls_section(
-			'content_section',
-			[
-				'label' => __( 'Content', 'wpt_pro' ),
-				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
-			]
-		);
+                //For General/Content Tab
+		$this->content_general();
                 
-                $args = array(
-                    'post_type' => 'wpt_product_table',
-                    'posts_per_page'=> '-1',
-                    'post_status' => 'publish',
-                );
-                $productTable = new WP_Query( $args );
-                $table_options = array();
-                $wpt_extra_msg = false;
-                if ($productTable->have_posts()) : 
-                    
-                    while ($productTable->have_posts()): $productTable->the_post();
-
-                    $id = get_the_id();
-                    $table_options[$id] = get_the_title();
-                    endwhile;
-                    //$table_options[''] = esc_html__( 'Please Choose a Table', 'wpt_pro' );
-                else:
-                    $table_options = false;
-                    //\Elementor\Controls_Manager::HEADING
-                endif;
+                //For Typography Section Style Tab
+                $this->style_table_head();
                 
-		
-                wp_reset_postdata();
-                wp_reset_query();
-                if( $table_options && is_array( $table_options ) ){
-                    $this->add_control(
-                            'table_id',
-                            [
-                                    'label' => __( 'Table List', 'wpt_pro' ),
-                                    'type' => \Elementor\Controls_Manager::SELECT,
-                                    'options' => $table_options,
-                                    //'default' => '',
-                            ]
-                    );
-                    /****************************
-                    $this->add_control(
-                            'table_edit',
-                            [
-				'label' => __( 'Additional Info', 'wpt_pro' ),
-				'type' => \Elementor\Controls_Manager::RAW_HTML,
-				'raw' => sprintf( 
-                                        __( 'Edit your %sTable%s.', 'wpt_pro' ), 
-                                        '<a href="' . esc_attr( admin_url( 'post.php?post=' . $table_ID . '&action=edit&classic-editor' ) ) . '">',
-                                        '</a>'
-                                        ),
-				'content_classes' => 'wpt_add_new_table',
-                            ]
-                    );
-                    //***************************************/
-                }else{
-                    $wpt_extra_msg = __( 'There is not founded any table to your. ', 'wpt_pro' );
-                }
+                //For Typography Section Style Tab
+                $this->style_table_body();
                 
-                $this->add_control(
-                        'table_notification',
-                        [
-                            'label' => __( 'Additional Information', 'wpt_pro' ),
-                            'type' => \Elementor\Controls_Manager::RAW_HTML,
-                            'raw' => $wpt_extra_msg . sprintf( 
-                                    __( 'Create %sa new table%s.', 'wpt_pro' ), 
-                                    '<a href="' . admin_url( 'post-new.php?post_type=wpt_product_table' ) . '">',
-                                    '</a>'
-                                    ),
-                            'content_classes' => 'wpt_elementor_additional_info',
-                        ]
-                );
                 
-		$this->end_controls_section();
-
 	}
 
 	/**
@@ -173,5 +117,216 @@ class WPT_Elementor_Widget extends \Elementor\Widget_Base{
                 echo '</h2>';
             }
 	}
+        
+        protected function content_general() {
+                $this->start_controls_section(
+			'general',
+			[
+				'label' => __( 'General', 'wpt_pro' ),
+				'tab' => Controls_Manager::TAB_CONTENT,
+			]
+		);
+                
+                $args = array(
+                    'post_type' => 'wpt_product_table',
+                    'posts_per_page'=> '-1',
+                    'post_status' => 'publish',
+                );
+                $productTable = new WP_Query( $args );
+                $table_options = array();
+                $wpt_extra_msg = false;
+                if ($productTable->have_posts()) : 
+                    
+                    while ($productTable->have_posts()): $productTable->the_post();
+
+                    $id = get_the_id();
+                    $table_options[$id] = get_the_title();
+                    endwhile;
+                    //$table_options[''] = esc_html__( 'Please Choose a Table', 'wpt_pro' );
+                else:
+                    $table_options = false;
+                    //Controls_Manager::HEADING
+                endif;
+                
+		
+                wp_reset_postdata();
+                wp_reset_query();
+                if( $table_options && is_array( $table_options ) ){
+                    $this->add_control(
+                            'table_id',
+                            [
+                                    'label' => __( 'Table List', 'wpt_pro' ),
+                                    'type' => Controls_Manager::SELECT,
+                                    'options' => $table_options,
+                                    //'default' => '',
+                            ]
+                    );
+                    /****************************
+                    $this->add_control(
+                            'table_edit',
+                            [
+				'label' => __( 'Additional Info', 'wpt_pro' ),
+				'type' => Controls_Manager::RAW_HTML,
+				'raw' => sprintf( 
+                                        __( 'Edit your %sTable%s.', 'wpt_pro' ), 
+                                        '<a href="' . esc_attr( admin_url( 'post.php?post=' . $table_ID . '&action=edit&classic-editor' ) ) . '">',
+                                        '</a>'
+                                        ),
+				'content_classes' => 'wpt_add_new_table',
+                            ]
+                    );
+                    //***************************************/
+                }else{
+                    $wpt_extra_msg = __( 'There is not founded any table to your. ', 'wpt_pro' );
+                }
+                
+                $this->add_control(
+                        'table_notification',
+                        [
+                            'label' => __( 'Additional Information', 'wpt_pro' ),
+                            'type' => Controls_Manager::RAW_HTML,
+                            'raw' => $wpt_extra_msg . sprintf( 
+                                    __( 'Create %sa new table%s.', 'wpt_pro' ), 
+                                    '<a href="' . admin_url( 'post-new.php?post_type=wpt_product_table' ) . '">',
+                                    '</a>'
+                                    ),
+                            'content_classes' => 'wpt_elementor_additional_info',
+                        ]
+                );
+                
+		$this->end_controls_section();
+
+        }
+        
+        /**
+         * Typography Section for Style Tab
+         * 
+         * @since 1.0.0.9
+         */
+        protected function style_table_head() {
+            $this->start_controls_section(
+                'thead',
+                [
+                    'label'     => esc_html__( 'Table Head', 'medilac' ),
+                    'tab'       => Controls_Manager::TAB_STYLE,
+                ]
+            );
+
+            $this->add_group_control(
+                    Group_Control_Typography::get_type(),
+                    [
+                            'name' => 'thead_typography',
+                            'global' => [
+                                    'default' => Global_Typography::TYPOGRAPHY_PRIMARY,
+                            ],
+                            'selector' => '{{WRAPPER}} table.wpt_product_table thead tr th',
+                    ]
+            );
+
+            $this->add_control(
+                'thead-color',
+                [
+                    'label'     => __( 'Color', 'medilac' ),
+                    'type'      => Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} table.wpt_product_table thead tr th' => 'color: {{VALUE}}',
+                    ],
+                    'default'   => '#ffffff',
+                ]
+            );
+            
+            $this->add_control(
+                'thead-bg-color',
+                [
+                    'label'     => __( 'Background Color', 'medilac' ),
+                    'type'      => Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} table.wpt_product_table thead tr th' => 'background-color: {{VALUE}}',
+                    ],
+                    'default'   => '#0a7f9c',
+                ]
+            );
+            
+            
+            
+            $this->end_controls_section();
+        }
+    
+        
+        
+        /**
+         * Typography Section for Style Tab
+         * 
+         * @since 1.0.0.9
+         */
+        protected function style_table_body() {
+            
+            
+            $this->start_controls_section(
+                'tbody',
+                [
+                    'label'     => esc_html__( 'Table Body', 'medilac' ),
+                    'tab'       => Controls_Manager::TAB_STYLE,
+                ]
+            );
+
+            $this->add_group_control(
+                    Group_Control_Typography::get_type(),
+                    [
+                            'name' => 'tbody_typography',
+                            'global' => [
+                                    'default' => Global_Typography::TYPOGRAPHY_PRIMARY,
+                            ],
+                            'selectors' => [
+                                '{{WRAPPER}} table.wpt_product_table tbody tr td',
+                                '{{WRAPPER}} table.wpt_product_table tbody tr td a',
+                                '{{WRAPPER}} table.wpt_product_table tbody tr td p',
+                                '{{WRAPPER}} table.wpt_product_table tbody tr td div',
+                            ],
+                    ]
+            );
+
+            $this->add_control(
+                'tbody-text-color',
+                [
+                    'label'     => __( 'Text Color', 'medilac' ),
+                    'type'      => Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} table.wpt_product_table tbody tr td' => 'color: {{VALUE}}',
+                        '{{WRAPPER}} table.wpt_product_table tbody tr td p' => 'color: {{VALUE}}',
+                        '{{WRAPPER}} table.wpt_product_table tbody tr td div' => 'color: {{VALUE}}',
+                    ],
+                    'default'   => '#535353',
+                ]
+            );
+            
+            $this->add_control(
+                'tbody-title-color',
+                [
+                    'label'     => __( 'Product Title Color', 'medilac' ),
+                    'type'      => Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} table.wpt_product_table tbody tr td .product_title a' => 'color: {{VALUE}}',
+                    ],
+                    'default'   => '#000',
+                ]
+            );
+            
+            
+            $this->add_control(
+                'tbody-bg-color',
+                [
+                    'label'     => __( 'Background Color', 'medilac' ),
+                    'type'      => Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} table.wpt_product_table tbody tr td' => 'background-color: {{VALUE}}',
+                    ],
+                    //'default'   => '#fff',
+                ]
+            );
+            
+            $this->end_controls_section();
+        }
+    
 
 }
