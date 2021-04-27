e17c6f6d483da3f5a18df3fa3cc895813302521a
Saiful Islam <codersaiful@gmail.com>
2021-04-27 12:16:25 +0600

    focus cart after search issue has solved

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1210,7 +1210,7 @@
             var temp_number = thisID.replace('single_keyword_','');
             $('#wpt_query_search_button_' + temp_number).trigger('click');
         }));
-        $('body').on('change','.search_select,.query_box_direct_value',function(){
+        $('body').on('change,focus','.search_select,.query_box_direct_value',function(){
             var thisID = $(this).parents('.wpt_product_table_wrapper').attr('id');
             var temp_number = thisID.replace('table_id_','');
            $('#wpt_query_search_button_' + temp_number).trigger('click');
