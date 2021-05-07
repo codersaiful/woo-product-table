e0c5ccdfb8d55eb71d5995069f6e0873094f897b
Saiful Islam <codersaiful@gmail.com>
2021-05-08 01:01:30 +0600

    version updated 2.8.7.0

--- woo-product-table.php
+++ woo-product-table.php
@@ -7,7 +7,7 @@
  * Author URI: https://codecanyon.net/user/codeastrology
  * Tags: woocommerce product list,woocommerce product table, wc product table, product grid view, inventory, shop product table
  * 
- * Version: 2.8.6
+ * Version: 2.8.7
  * Requires at least:    4.0.0
  * Tested up to:         5.7.1
  * WC requires at least: 3.0.0
@@ -30,7 +30,7 @@ if( !defined( 'WPT_PLUGIN_BASE_FOLDER' ) ){
 }
 
 if( !defined( 'WPT_DEV_VERSION' ) ){
-    define( 'WPT_DEV_VERSION', '2.8.6.5' );
+    define( 'WPT_DEV_VERSION', '2.8.7.0' );
 }
 
 if( !defined( 'WPT_CAPABILITY' ) ){
