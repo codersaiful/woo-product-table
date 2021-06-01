e6c3f1a489510e102b48bf6dcf3291bb2ef1172b
Saiful Islam <codersaiful@gmail.com>
2021-06-01 10:36:47 +0600

    comment code and vardump removed from includes/check.php file

--- includes/items/check.php
+++ includes/items/check.php
@@ -9,48 +9,8 @@ $check_class_arr = array(
     "wpt_inside_check_{$temp_number}",
 );
 $check_class = implode(" ", $check_class_arr);
-var_dump($checkbox);        
-            
-$wpt_single_check = false;
-$wpt_single_check .= "<input "
-    . "data-product_type='" . $product->get_type() . "' "
-    . "id='check_id_{$temp_number}_" . $data['id'] . "' "
-    . "data-temp_number='{$temp_number}' "
-    . "data-product_id='" . $data['id'] . "' "
-    . "class='" . esc_attr( $check_class ) . "'"
-    . "type='checkbox' "
-    . "value='0'"
-    . ( $checkbox == 'wpt_checked_table' && $enable_disable == 'enabled' ? " checked='checked'" : "" )
-    . ">"
-    . "<label for='check_id_{$temp_number}_" . $data['id'] . "'></label>";
-    
-$allowed_atts = array(
-        'value'      => true,
-        'type'      => true,
-        'class'      => true,
-        'data-product_id'      => true,
-        'data-temp_number'      => true,
-        'id'      => true,
-        'data-product_type'      => true,
-        'for'      => true,
-);
 
-$allowed_tags['input']     = $allowed_atts;
-//$allowed_tags['label']     = $allowed_atts;
-//$allowed_tags['div']     = $allowed_atts;
-//var_dump($allowed_tags);
-//echo $wpt_single_check;//
-//
-//$allowed_html = array(
-//  'a' => array(
-//    'href' => array(),
-//  ),
-//  'br' => array(),
-//);
-//var_dump($wpt_single_check);
-//echo wp_kses( $wpt_single_check, $allowed_tags );
-//echo wp_kses( $wpt_single_check, $allowed_tags );
-//var_dump(wp_kses_allowed_html()); ?>    
+?>    
 <input 
     data-product_type='<?php echo esc_attr( $product->get_type() ); ?>' 
     id='<?php echo esc_attr( "check_id_{$temp_number}_{$data['id']}" ); ?>' 
