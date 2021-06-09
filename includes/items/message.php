a86b56ca76fc10949867551fd16426b0b935fb7e
Saiful Islam <codersaiful@gmail.com>
2021-06-09 17:45:08 +0600

    by html

--- includes/items/message.php
+++ includes/items/message.php
@@ -1,4 +1,5 @@
-<?php
-echo "<input type='text' class='message message_{$id}' id='message_{$id}' placeholder='" . $config_value['type_your_message'] . "'>";
-
-           
+<input type='text' 
+       class='message message_<?php echo esc_attr( $id ); ?>' 
+       value='' 
+       id='message_<?php echo esc_attr( $id ); ?>' 
+       placeholder='<?php echo esc_attr( $config_value['type_your_message'] ); ?>'>     
