bc70e5f8e2d7e657ddc44365b1858cd733f74d41
Saiful Islam <codersaiful@gmail.com>
2021-07-15 22:29:53 +0600

    WC tested up to: 	 5.5.1

--- woo-product-table.php
+++ woo-product-table.php
@@ -11,7 +11,7 @@
  * Requires at least:    4.0.0
  * Tested up to:         5.7.8
  * WC requires at least: 3.0.0
- * WC tested up to: 	 5.4.1
+ * WC tested up to: 	 5.5.1
  * 
  * Text Domain: wpt_pro
  * Domain Path: /languages/
