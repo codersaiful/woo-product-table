aa825e9575683aed2a2cdd28b3944a47ddf351dc
Saiful Islam <codersaiful@gmail.com>
2021-08-11 15:40:57 +0600

    'WPT_DEV_VERSION', '2.9.8.0'

--- woo-product-table.php
+++ woo-product-table.php
@@ -7,7 +7,7 @@
  * Author URI: https://profiles.wordpress.org/codersaiful/
  * Tags: woocommerce product list,woocommerce product table, wc product table, product grid view, inventory, shop product table
  * 
- * Version: 2.9.7
+ * Version: 2.9.8
  * Requires at least:    4.0.0
  * Tested up to:         5.8
  * WC requires at least: 3.0.0
@@ -30,7 +30,7 @@ if( !defined( 'WPT_PLUGIN_BASE_FOLDER' ) ){
 }
 
 if( !defined( 'WPT_DEV_VERSION' ) ){
-    define( 'WPT_DEV_VERSION', '2.9.7.0' );
+    define( 'WPT_DEV_VERSION', '2.9.8.0' );
 }
 
 if( !defined( 'WPT_CAPABILITY' ) ){
