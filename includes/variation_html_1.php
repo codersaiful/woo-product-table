07e5049b66afe13fbe345ec7a17f25c38848670f
Saiful Islam <codersaiful@gmail.com>
2021-08-30 16:44:23 +0600

    variation back

--- includes/variation_html_1.php
+++ includes/variation_html_1.php
@@ -6,10 +6,13 @@ global $product;
 $attribute_keys  = array_keys( $attributes );
 $variations_json = wp_json_encode( $available_variations );
 $variations_attr = function_exists( 'wc_esc_json' ) ? wc_esc_json( $variations_json ) : _wp_specialchars( $variations_json, ENT_QUOTES, 'UTF-8', true );
+
+do_action( 'woocommerce_before_add_to_cart_form' );
 ?>
 
 <form class="variations_form cart" action="" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo $variations_attr; // WPCS: XSS ok. ?>">
-<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
+    <?php do_action( 'woocommerce_before_variations_form' ); ?>
+    <?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
         <p class="stock out-of-stock"><?php echo esc_html( apply_filters( 'woocommerce_out_of_stock_message', __( 'This product is currently out of stock and unavailable.', 'woocommerce' ) ) ); ?></p>
 <?php else : ?>
         
@@ -55,7 +58,33 @@ $variations_attr = function_exists( 'wc_esc_json' ) ? wc_esc_json( $variations_j
                 
         <?php endforeach; ?>
         </div>
+        <div class="single_variation_wrap">
+			<?php
+				/**
+				 * Hook: woocommerce_before_single_variation.
+				 */
+				do_action( 'woocommerce_before_single_variation' );
+
+				/**
+				 * Hook: woocommerce_single_variation. Used to output the cart button and placeholder for variation data.
+				 *
+				 * @since 2.4.0
+				 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
+				 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
+				 */
+				do_action( 'woocommerce_single_variation' );
+
+				/**
+				 * Hook: woocommerce_after_single_variation.
+				 */
+				do_action( 'woocommerce_after_single_variation' );
+			?>
+		</div>
+        
 <?php endif; ?>
+        <?php do_action( 'woocommerce_after_variations_form' ); ?>
 </form>
 <?php
+do_action( 'woocommerce_after_add_to_cart_form' );
+
 //do_action( 'wpt_action_variation', $product ); //Sepcially for Min Max Plugin
