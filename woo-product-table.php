dfd891915a658cc86c2532c02dcfa7ad61e97593
Saiful Islam <codersaiful@gmail.com>
2021-09-05 11:56:43 +0600

    autor name and tested update upto checked

--- woo-product-table.php
+++ woo-product-table.php
@@ -3,7 +3,7 @@
  * Plugin Name: Product Table for WooCommerce - (wooproducttable)
  * Plugin URI: https://codecanyon.net/item/woo-product-table-pro/20676867?ref=CodeAstrology&utm_source=WPT_Installed_Plugin
  * Description: WooCommerce all products display as a table in one page by shortcode. Fully responsive and mobile friendly. Easily customizable - color,background,title,text color etc.
- * Author: Saiful Islam
+ * Author: CodeAstrology
  * Author URI: https://profiles.wordpress.org/codersaiful/
  * Tags: woocommerce product list,woocommerce product table, wc product table, product grid view, inventory, shop product table
  * 
@@ -11,7 +11,7 @@
  * Requires at least:    4.0.0
  * Tested up to:         5.8
  * WC requires at least: 3.0.0
- * WC tested up to: 	 5.5.2
+ * WC tested up to: 	 5.6.0
  * 
  * Text Domain: wpt_pro
  * Domain Path: /languages/
