3a701241f00d13db3f4f12cfd21718b0ed746de6
Saiful Islam <codersaiful@gmail.com>
2021-10-11 16:47:40 +0600

    $message_type = apply_filters( 'wpto_short_message_box', 'input', $settings, $product, $keyword, $table_ID, $column_settings ); #iitems/message.php

--- includes/items/message.php
+++ includes/items/message.php
@@ -1,5 +1,24 @@
+<?php
+$message_type = apply_filters( 'wpto_short_message_box_type', 'input', $settings, $product, $keyword, $table_ID, $column_settings );
+
+if($message_type == 'input'){
+?>
+    
 <input type='text' 
        class='message message_<?php echo esc_attr( $id ); ?>' 
        value='' 
        id='message_<?php echo esc_attr( $id ); ?>' 
-       placeholder='<?php echo esc_attr( $config_value['type_your_message'] ); ?>'>     
+       placeholder='<?php echo esc_attr( $config_value['type_your_message'] ); ?>'>   
+<?php
+}else{
+?>
+<textarea
+    class='message message_<?php echo esc_attr( $id ); ?>' 
+       value='' 
+       id='message_<?php echo esc_attr( $id ); ?>' 
+       placeholder='<?php echo esc_attr( $config_value['type_your_message'] ); ?>'
+    
+    ></textarea>    
+<?php    
+}
+  
