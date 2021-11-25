4a320c456e83733c196da836bf88b4de32155865
Saiful Islam <codersaiful@gmail.com>
2021-11-25 15:58:20 +0600

    #custom_field.php inside includes/items - if( $product->get_type() == 'variation' && !$variation_wise_id

--- includes/items/custom_field.php
+++ includes/items/custom_field.php
@@ -5,10 +5,10 @@ $settings = isset( $column_settings[$keyword] ) ? $column_settings[$keyword] : f
 $generated_keyword = isset( $settings['older'] ) && $settings['older'] == true ? substr( $keyword, 3 ) : $keyword;
 
 $product_id = $id;
- $variation_wise_id = apply_filters( 'wpto_product_real_id', false, $product, $temp_number, $keyword, $column_settings );
- if( $product->get_type() == 'variation' && !$variation_wise_id ){ // && !$variation_wise_id
-     $product_id = $product->get_parent_id();
- }
+// $variation_wise_id = apply_filters( 'wpto_product_real_id', false, $product, $temp_number, $keyword, $column_settings );
+// if( $product->get_type() == 'variation' && !$variation_wise_id ){ // && !$variation_wise_id
+//     $product_id = $product->get_parent_id();
+// }
  $customfield_content = false;
  $custom_meta = get_post_meta( $product_id, $generated_keyword, true);
 
