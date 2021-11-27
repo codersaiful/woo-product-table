44b8616f3aacab15f2d9b9b8f84fc46742e2d1d6
Saiful Islam <codersaiful@gmail.com>
2021-11-27 13:17:30 +0600

    (exdays*24*60*60*1000)

--- admin/notice/offeres/offer.js
+++ admin/notice/offeres/offer.js
@@ -97,7 +97,7 @@
 
         function setCookie(cname, cvalue, exdays) {
             const d = new Date();
-            d.setTime(d.getTime() + 20000); //(exdays*24*60*60*1000)
+            d.setTime(d.getTime() + (exdays*24*60*60*1000)); //(exdays*24*60*60*1000)
             let expires = "expires="+ d.toUTCString();
             document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
         }
