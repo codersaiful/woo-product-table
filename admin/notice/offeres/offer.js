1648cda4918f826995b6bce707f892e722ff3e48
Saiful Islam <codersaiful@gmail.com>
2021-11-27 13:06:35 +0600

    fully done

--- admin/notice/offeres/offer.js
+++ admin/notice/offeres/offer.js
@@ -4,38 +4,53 @@
         if(! $('body').hasClass('wpt_admin_body')){
             return false;
         }
-        
-        $(document.body).on('click','h1.wp-heading-inline',function(){
-            loadOfferContent();
-        });
-        loadOfferContent();
 
+        let wrapper_class = '.wpt-special-offer';
+        loadOfferContent();
+        
         function loadOfferContent(){
+            let offer_json = getCookie('wpt_offer_latest');
+            if( offer_json !== '' ){
+                console.log(offer_json);
+                offer_json = JSON.parse(offer_json);
+                jsonToHtmlMarkup(offer_json);
+
+            }else if(navigator.onLine){
+                loadContentByAjax();
+            }
+            
+        }
+
+        function loadContentByAjax(){
             let url = "https://raw.githubusercontent.com/codersaiful/woo-product-table/3.0.4.2/admin/notice/offeres/offer.json";
             $.ajax({
                 url: url,
                 method: "GET",
                 dataType: 'json',
                 success:function(result){
-                    jsonToHtmlMarkup(result);
+                    if(result && result !== ''){
+                        setCookie('wpt_offer_latest',JSON.stringify(result),2);//For Two days
+                        jsonToHtmlMarkup(result);
+                    }
                     
-                    
-                    console.log(result);
                 },
-                fail:function(){
-                    alert(2323);
+                error:function(e){
+                    console.log("Something went wrong.");
                 }
             });
         }
 
         function jsonToHtmlMarkup(result){
-            let wrapper_class = '.wpt-special-offer',
-            offer = result.offer,
+            if(! result.offer.status){
+                return;
+            }
+            let offer = result.offer,
             image_url = offer.image_url,
             target_link = offer.target_link,
             target_html = offer.target_html,
             wrapper_css = offer.wrapper_css,
             image_css = offer.image_css;
+            
 
             let location_selector = offer.display_wrapper + " " + offer.display_to;
 
@@ -59,9 +74,31 @@
             $(wrapper_class).css(wrapper_css);
 
             if(image_css){
-                console.log($(wrapper_class),image_css);
                 $(wrapper_class).find("img.offer_image").css(image_css);
             }
+        }
+
+        function getCookie(cname) {
+            let name = cname + "=";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for(let i = 0; i <ca.length; i++) {
+              let c = ca[i];
+              while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+              }
+              if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+              }
+            }
+            return "";
+        }
+
+        function setCookie(cname, cvalue, exdays) {
+            const d = new Date();
+            d.setTime(d.getTime() + 20000); //(exdays*24*60*60*1000)
+            let expires = "expires="+ d.toUTCString();
+            document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
         }
 
     });    
