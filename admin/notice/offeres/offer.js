6be66f012e1d038d1fa668cb24cf0504cdc41b3d
Saiful Islam <codersaiful@gmail.com>
2021-11-27 12:03:35 +0600

    working for blkf

--- admin/notice/offeres/offer.js
+++ admin/notice/offeres/offer.js
@@ -16,25 +16,48 @@
                 method: "GET",
                 dataType: 'json',
                 success:function(result){
-                    let offer = result.offer;
-                    let image_url = offer.image_url;
-                    let target_link = offer.target_link;
-                    let target_html = offer.target_html;
-                    let selector = offer.display_wrapper + " " + offer.display_to;
-                    let html = `
-                    <div class="wpt-special-offer">
-                        <a href="`+ target_link +`" target="_blank">
-                            <img src="` + image_url + `">
-                        </a>
-                    </div>
-                    `;
-                    $(selector).prepend(html);
-                    console.log(result,selector);
+                    jsonToHtmlMarkup(result);
+                    
+                    
+                    console.log(result);
                 },
                 fail:function(){
                     alert(2323);
                 }
             });
         }
+
+        function jsonToHtmlMarkup(result){
+            let wrapper_class = '.wpt-special-offer',
+            offer = result.offer,
+            image_url = offer.image_url,
+            target_link = offer.target_link,
+            target_html = offer.target_html,
+            wrapper_css = offer.wrapper_css,
+            image_css = offer.image_css;
+
+            let location_selector = offer.display_wrapper + " " + offer.display_to;
+
+            $(wrapper_class).remove();
+
+            let image_link_html = "";
+            if( image_url && target_link ){
+                image_link_html = `<a class="offer_image_link" href="`+ target_link +`" target="_blank">
+                    <img class="offer_image" src="` + image_url + `">
+                </a>`;
+            }
+
+            if(!target_html){
+                target_html=""
+            }
+
+            let html = `<div class="wpt-special-offer">` + image_link_html + target_html + `</div>`;
+            if(image_css){
+                $(wrapper_class).find("img.offer_image").css(image_css);
+            }
+            $(location_selector).prepend(html);
+            $(wrapper_class).css(wrapper_css);
+        }
+
     });    
 })(jQuery);
