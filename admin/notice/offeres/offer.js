2ce52b4e81cfd230a2da583d0928c1fc7dc0c0f3
Saiful Islam <codersaiful@gmail.com>
2021-11-27 11:46:48 +0600

    working

--- admin/notice/offeres/offer.js
+++ admin/notice/offeres/offer.js
@@ -4,17 +4,37 @@
         if(! $('body').hasClass('wpt_admin_body')){
             return false;
         }
-        let url = "https://raw.githubusercontent.com/codersaiful/woo-product-table/3.0.4.2/admin/notice/offeres/offer.json";
-        $.ajax({
-            url: url,
-            method: "GET",
-            dataType: 'json',
-            success:function(result){
-                console.log(result);
-            },
-            fail:function(){
-                alert(2323);
-            }
+        
+        $(document.body).on('click','h1.wp-heading-inline',function(){
+            loadOfferContent();
         });
+
+        function loadOfferContent(){
+            let url = "https://raw.githubusercontent.com/codersaiful/woo-product-table/3.0.4.2/admin/notice/offeres/offer.json";
+            $.ajax({
+                url: url,
+                method: "GET",
+                dataType: 'json',
+                success:function(result){
+                    let offer = result.offer;
+                    let image_url = offer.image_url;
+                    let target_link = offer.target_link;
+                    let target_html = offer.target_html;
+                    let selector = offer.display_wrapper + " " + offer.display_to;
+                    let html = `
+                    <div class="wpt-special-offer">
+                        <a href="`+ target_link +`" target="_blank">
+                            <img src="` + image_url + `">
+                        </a>
+                    </div>
+                    `;
+                    $(selector).prepend(html);
+                    console.log(result,selector);
+                },
+                fail:function(){
+                    alert(2323);
+                }
+            });
+        }
     });    
 })(jQuery);
