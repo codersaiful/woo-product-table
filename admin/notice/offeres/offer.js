bf3855d3bab271ee414ce9b1a768da2bf52b1a3f
Saiful Islam <codersaiful@gmail.com>
2021-11-27 11:29:30 +0600

    still working

--- admin/notice/offeres/offer.js
+++ admin/notice/offeres/offer.js
@@ -4,8 +4,17 @@
         if(! $('body').hasClass('wpt_admin_body')){
             return false;
         }
-
-
-        alert(2332);
+        let url = "https://raw.githubusercontent.com/codersaiful/woo-product-table/3.0.4.2/admin/notice/offeres/offer.json";
+        $.ajax({
+            url: url,
+            method: "GET",
+            dataType: 'json',
+            success:function(result){
+                console.log(result);
+            },
+            fail:function(){
+                alert(2323);
+            }
+        });
     });    
 })(jQuery);
