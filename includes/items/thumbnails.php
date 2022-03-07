610e721a282a0b3b6e49c5cbe114f64ecc9f1572
Md Rakibul Islam <freelancerakibul@gmail.com>
2022-03-07 12:42:25 +0600

    thumbnail gallery moved to new branch

--- includes/items/thumbnails.php
+++ includes/items/thumbnails.php
@@ -1,11 +1,7 @@
 <?php
 $wpt_single_thumbnails = false;
 $thumb_variation = isset( $column_settings['thumb_variation'] ) ? $column_settings['thumb_variation'] : 'popup';
-$thumb_gallery = isset( $column_settings['thumbnails']['thumb_gallery'] ) ? 'no' : 'yes';
-
-var_dump($thumb_gallery);
-
-$img_src = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'full', false );   
+$img_src = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'full', false );
 $img_attr = $img_src ? " data-url='{$img_src[0]}' data-width='{$img_src[1]}' data-height='{$img_src[2]}' " : '';
 
 $thumbs_size = apply_filters( 'wpto_thumbs_size', array( $config_value['thumbs_image_size'], $config_value['thumbs_image_size'] ), $settings, $column_settings, $table_ID, $product );
@@ -16,7 +12,7 @@ if($thumb_variation == 'quick_view'){
     $thumb_variation = 'quick_view yith-wcqv-button';
     $tag_start = "<div data-product_id='{$id}' class=' {$thumb_variation} '>";
     $tag_end = "</div>";
-    
+
 }elseif($thumb_variation == 'url'){
     $thumbs_img = "<a href='" . esc_url(get_the_permalink()) . "' target='{$config_value['product_link_target']}'>" . $thumbs_img . '</a>';
 }
