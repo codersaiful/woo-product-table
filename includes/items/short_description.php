d75d7b1d495370f043fcaf02a64162c288d4f3e9
Saiful Islam <codersaiful@gmail.com>
2022-03-22 08:14:05 +0600

    escapping in short description

--- includes/items/short_description.php
+++ includes/items/short_description.php
@@ -1,6 +1,6 @@
 <?php
 
-$desc = $data['short_description'];
+$desc = isset( $data['short_description'] ) ? sanitize_post( $data['short_description'] ) : '';
 if( ! empty( $desc ) ){
     echo do_shortcode( $desc );
 }
