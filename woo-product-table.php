5c9105ddd374b4fa46460a09d052ff8e9ce1bc8a
Saiful Islam <codersaiful@gmail.com>
2022-03-22 14:48:47 +0600

    'WPT_DEV_VERSION', '3.1.2.3'

--- woo-product-table.php
+++ woo-product-table.php
@@ -30,7 +30,7 @@ if( !defined( 'WPT_PLUGIN_BASE_FOLDER' ) ){
 }
 
 if( !defined( 'WPT_DEV_VERSION' ) ){
-    define( 'WPT_DEV_VERSION', '3.1.2.0' );
+    define( 'WPT_DEV_VERSION', '3.1.2.3' );
 }
 
 if( !defined( 'WPT_CAPABILITY' ) ){
