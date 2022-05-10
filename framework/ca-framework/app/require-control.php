ae136f30812c29716f098998ad688ead452ffda7
Saiful Islam <codersaiful@gmail.com>
2022-05-10 16:32:52 +0600

    framework updated

--- framework/ca-framework/app/require-control.php
+++ framework/ca-framework/app/require-control.php
@@ -91,7 +91,10 @@ if( ! class_exists( 'CA_Framework\Require_Control' ) ){
             if( is_plugin_active( $this->plugin_slug ) ) return;
 
             //Return Null Controll;
-            if( isset( $_GET['action'] ) && ( $_GET['action'] == 'install-plugin' || $_GET['action'] == 'activate' ) ) return;
+            if( isset( $_GET['action'] ) && ( $_GET['action'] == 'install-plugin' || $_GET['action'] == 'activate' ) ){
+                $this->stop_next = 1;
+                return $this->stop_next;
+            }
 
             //Check Aganin installation prosibility when reconneded and Date over. by default we set diff_limit = 5 days.
             if( ! $this->required && $this->repeat_display() ) return;
