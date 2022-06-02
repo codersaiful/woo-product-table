c822dc15eb601c6400f3ea13fe1c604e7f861d72
Fazle Bari <fazlebarisn@gmail.com>
2022-06-02 17:50:26 +0600

    fix total quantity for quick quantity

--- includes/items/total.php
+++ includes/items/total.php
@@ -39,6 +39,26 @@ $total_price = !empty( get_post_meta($product->get_id(), $wholesale_meta_key, tr
  * @by Saiful
  */
 $total_price = apply_filters( 'wpto_flat_product_price', $total_price, $product->get_id(), get_current_user_id(), $user_role, $product );
+
+$cart = WC()->cart->get_cart();
+
+$product_cart_id = WC()->cart->generate_cart_id( $product->get_id() );
+
+if( WC()->cart->find_product_in_cart( $product_cart_id )) {
+    $quantity = $cart[$product_cart_id]['quantity'];
+}
+
+//$product_price = (int) $product->get_price();
+$product_price = !empty( get_post_meta($product->get_id(), $wholesale_meta_key, true) ) ? get_post_meta($product->get_id(), $wholesale_meta_key, true) : $product->get_price();
+
+// Get the quantity if targeted product is in cart
+if( isset( $quantity ) && $quantity > 0 ) {
+    $total_price = ( $quantity * $product_price );
+}
+
+
+//var_dump( $quantity );
+
 echo "<div data-number_of_decimal='" . esc_attr( $number_of_decimal ) . "' "
         . "data-thousand_separator='" . esc_attr( $thousand_separator ) . "' "
         . "data-price_decimal_separator='" . esc_attr( $price_decimal_separator ) . "' "
@@ -47,3 +67,6 @@ echo "<div data-number_of_decimal='" . esc_attr( $number_of_decimal ) . "' "
         . "data-price_format='". esc_attr( $priceFormat ) ."' "
         . "class='wpt_total_item wpt_total " . ( $variable_for_total || !$total_price ? 'total_variaion' : 'total_general' ) . "'>"
         . "<strong>" . ( !$variable_for_total ? wp_kses_post( $newPrice ) : '' ) . "</strong></div>";
+
+
+$quantity = '';
