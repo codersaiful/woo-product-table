0d2e0e38562e64e77083cbc99b08e4a8b4efc7b0
Saiful Islam <codersaiful@gmail.com>
2022-08-23 09:44:04 +0600

    Quick View recommended error fix

--- framework/recommeded.php
+++ framework/recommeded.php
@@ -21,7 +21,8 @@ class Recommeded
         if( method_exists($req_qv, 'set_location') ){
             $req_qv->set_location('wpto_column_setting_form_quick_view'); //wpt_premium_image_bottom
             // $req_qv->set_location('wpto_column_setting_form_inside_quick_view'); //wpt_premium_image_bottom
+            $req_qv->run();
         }
-        $req_qv->run();
+        
     }
 }
