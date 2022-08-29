862465751958188852e5e8c1bbea15cad12212f5
Saiful Islam <codersaiful@gmail.com>
2022-08-29 12:07:09 +0600

    Table Head Done Actually

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -17,8 +17,15 @@ class Shortcode{
     public $table_type = 'normal_table';
 
     public $is_table;
+
+    /**
+     * Check column available or not, if empty array of _enable_cols, it will return false.
+     *
+     * @var boolean
+     */
+    public $is_table_head = true;
+    public $is_table_column = true;
     public $_device;
-    public $_config;
     public $args;
 
     public $_enable_cols;
@@ -43,7 +50,10 @@ class Shortcode{
     public $checkbox;
     public $template;
 
+    public $table_style;
+    public $_config;
     public $wrapper_class;
+    public $table_class;
 
     
 
@@ -51,6 +61,7 @@ class Shortcode{
         add_shortcode( $this->shortcde_text, [$this, 'shortcode'] );
     }
     public function shortcode($atts){
+        
         $this->atts = $atts;
 
         $pairs = array( 'exclude' => false );
@@ -69,7 +80,7 @@ class Shortcode{
         data-site_url="<?php echo esc_url( site_url() ); ?>" 
         id="table_id_<?php echo esc_attr( $this->table_id ); ?>" 
         class="<?php echo esc_attr( Table_Attr::wrapper_class( $this ) ); ?>">
-            <h1 class="test_title">Test Titlte</h1>
+            
             <div class="wpt_table_tag_wrapper">
                 <table 
                 data-page_number=""
@@ -80,6 +91,7 @@ class Shortcode{
                 id="wpt_table"
                 class="<?php echo esc_attr( Table_Attr::table_class( $this ) ); ?>">
 
+                <?php $this->table_head(); ?>
                 </table>
             </div>
 
@@ -126,7 +138,9 @@ class Shortcode{
 
 
 
-        if( empty( $this->_enable_cols ) ){
+        if( empty( $this->_enable_cols ) || ! is_array( $this->_enable_cols ) ){
+            $this->is_table_head = false;
+            $this->is_table_column = false;
             return Msg::not_found_cols($this);
         }
 
@@ -142,8 +156,8 @@ class Shortcode{
         $this->posts_per_page = $this->conditions['posts_per_page'] ?? $this->posts_per_page;
         $this->table_type = $this->conditions['table_type'] ?? $this->table_type;
 
-
         //Some Basic Meta Values | All other query related available in Args Class
+        $this->table_head = ! isset( $this->basics['table_head'] ) ? true : false;
         $this->minicart_position = $this->basics['minicart_position'] ?? '';
         $this->add_to_cart_text = $this->basics['add_to_cart_text'] ?? '';//$basics['add_to_cart_text'] ?? ''
         $this->pagination_ajax = $this->basics['pagination_ajax'] ?? '';
@@ -172,6 +186,31 @@ class Shortcode{
         return $this->shortcde_text;
     }
 
+    private function table_head(){
+        if( ! $this->table_head ) return;
+        if( ! $this->is_table_head ) return; //Check column available or not, if empty array of _enable_cols, it will return false.
+        
+        ?>
+        <thead>
+            <tr data-temp_number="<?php echo esc_attr( $this->table_id ); ?>" class="wpt_table_header_row wpt_table_head">
+            <?php 
+            foreach( $this->_enable_cols as $key => $col ){
+            $col_content = $this->column_array[$key] ?? $col;
+            if( $key == 'check' ){
+                $col_content = "<input data-type='universal_checkbox' data-temp_number='{$this->table_id}' class='wpt_check_universal' id='wpt_check_uncheck_column_{$this->table_id}' type='checkbox'><label for=wpt_check_uncheck_column_{$this->table_id}></label>";
+            }
+            ?>
+            <th class="<?php echo esc_attr( $key ); ?>">
+                <?php echo $col_content; ?>
+            </th>
+            <?php
+            }
+            ?>
+            </tr>
+        </thead>
+        <?php
+        
+    }
 
     /**
      * Getting meta value,
