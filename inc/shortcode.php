c90720889908b93a10cbae04612b8dea5e959974
Saiful Islam <codersaiful@gmail.com>
2022-09-28 13:13:11 +0600

    finall version done

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -400,16 +400,15 @@ class Shortcode extends Shortcode_Base{
             $display_count = ( $prev_post + 1 ) . " - $current_total_post";
         }
 
-        $stats_post_count = $this->basics['stats_post_count'] ?? '';
-        $stats_page_count = $this->basics['stats_page_count'] ?? '';
-        // var_dump( $stats_post_count, $stats_page_count );
+        $stats_post_count = $this->basics['stats_post_count'] ?? __( "Showing %s out of %s", "wpt_pro" );
+        $stats_page_count = $this->basics['stats_page_count'] ?? __( "Page %s out of %s", "wpt_pro" );
         
         ?>
         <p class="wpt-stats-post-count">
-            <?php printf( esc_html__( "Showing %s out of %s", "wpt_pro" ), $display_count, $this->found_posts  ); ?>
+            <?php printf( esc_html( $stats_post_count ), $display_count, $this->found_posts  ); ?>
         </p>
         <p class="wpt-stats-page-count">
-        <?php printf( esc_html__( "Page %s out of %s" ), $display_pagN, $this->max_num_pages  ); ?>
+        <?php printf( esc_html( $stats_page_count ), $display_pagN, $this->max_num_pages  ); ?>
         </p>
         <?php 
 
