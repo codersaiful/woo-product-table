81779d52a5ce75d32e8ee817a855e956202df60b
Saiful Islam <codersaiful@gmail.com>
2022-09-30 12:42:07 +0600

    working for Manage enabel column

--- inc/handle/enable-column.php
+++ inc/handle/enable-column.php
@@ -6,11 +6,12 @@ use WOO_PRODUCT_TABLE\Inc\Shortcode;
 
 class Enable_Column extends Base{
 
-    public static $shortcode;
-    public static $args;
-    public static $table_id;
-    public static $cols;
-
+    // public static $shortcode;
+    // public static $args;
+    // public static $table_id;
+    // public static $cols;
+    public static $inner_items = [];
+    public static $message_col = false;
 
 
     /**
@@ -28,16 +29,42 @@ class Enable_Column extends Base{
      * @author Saiful Islam <codersaiful@gmail.com>
      */
     public static function manage( Shortcode $shortcode ){
-        self::$table_id = $shortcode->table_id;
-        self::$cols = $shortcode->_enable_cols;
-        // var_dump($shortcode->req_product_type);
-        // var_dump($shortcode->table_type);
-        //var_dump( $shortcode->table_type == 'advance', $shortcode->table_type,$shortcode->_enable_cols['message'] );
-        if( isset( $shortcode->_enable_cols['message'] ) && $shortcode->table_type == 'advance_table' ){
-            unset($shortcode->_enable_cols['message']);
-            add_action( 'woocommerce_before_add_to_cart_quantity', 'wpt_add_custom_message_field' );
-        }
 
+
+        /**
+         * Handling Message Column or as Innter Item
+         * If found any where and Enable Advance Table/ Third party plugin support,
+         * 
+         * then I will add it by action hook inside action column. Otherwise, it will not work
+         * 
+         * @author Saiful Islam <codersaiful@gmail.com>
+         * @since 3.2.5.5.final8
+         */
+        if( $shortcode->table_type == 'advance_table' ){
+            self::$message_col = isset( $shortcode->_enable_cols['message'] ); //If available, value will be true
+            
+            self::$inner_items = array_column( $shortcode->column_settings, 'items' );
+
+            foreach( $shortcode->column_settings as $key=>$items){
+                if( ! isset( $items['items'] ) ) continue;
+
+                $m_index = array_search( 'message', $items['items'] );
+                
+                if( is_int( $m_index ) ){
+                    
+                    unset($shortcode->column_settings[$key]['items'][$m_index]);
+                    self::$message_col = true;
+                    break;
+                }
+                
+            }
+
+            if( self::$message_col ){
+                unset($shortcode->_enable_cols['message']);
+                add_action( 'woocommerce_before_add_to_cart_quantity', 'wpt_add_custom_message_field' );
+            }
+        }
+        //If set Only Product Variation
         if( $shortcode->req_product_type == 'product_variation' ){
             unset( $shortcode->_enable_cols['category'] );
             unset( $shortcode->_enable_cols['tags'] );
