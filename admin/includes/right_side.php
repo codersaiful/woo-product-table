54291e167a76703e2f5962488e4236977d89bfbf
Saiful Islam <codersaiful@gmail.com>
2022-10-06 17:59:51 +0600

    🥇HONORABLE CONTRIBUTOR added at sidebar

--- admin/includes/right_side.php
+++ admin/includes/right_side.php
@@ -1,6 +1,71 @@
 <div class="wpt_rightside">
     <div class="wpt_right_side_in_wrapper" style="padding: 10px;">
         <div class="social_title_wrapper">
+            <h3 class="social_title">🥇HONORABLE CONTRIBUTOR</h3>
+            
+            <div class="social_links">
+                <ul>
+                    <li><a href="https://github.com/codersaiful" rel="nofollow ugc">codersaiful</a> (1,000+ commits And Pro: 1000+ commits )</li>
+                    <li><a href="https://github.com/unikforceit" rel="nofollow ugc">unikforceit</a> (42 commits )</li>
+                    <li><a href="https://github.com/fatimakhatungit" rel="nofollow ugc">fatimakhatungit</a> (38 commits )</li>
+                    <li><a href="https://github.com/fazlebarisn" rel="nofollow ugc">fazlebarisn</a> (35 commits And Pro: 50+ commits)</li>
+                    <li><a href="https://github.com/rafiul" rel="nofollow ugc">rafiul</a> (29 commits )</li>
+                    <li><a href="https://github.com/tanyabouman" rel="nofollow ugc">tanyabouman</a> (3 commits )</li>
+                    <li>🥇 <a href="https://github.com/codersaiful/woo-product-table/fork" rel="nofollow ugc">You can join here</a></li>
+                </ul>
+                <strong>👷<a href='https://github.com/codersaiful/woo-product-table/graphs/contributors' target="_blank">Contributor List on GitHub</a></strong>
+            </div>
+        </div>
+    </div>
+    
+    <div class="wpt_right_side_in_wrapper" style="padding: 10px;">
+        <div class="need_help_wrapper">
+            <h1><?php echo esc_html__( 'Need Help?', 'wpt_pro' ); ?></h1>
+            <a style="display: block;text-align: left;" target="_blank" href="https://codeastrology.com/support/">
+                <img style="max-width: 100px" src="<?php echo WPT_BASE_URL; ?>assets/images/customer_support.png">
+                <h3><?php echo esc_html__( 'Customer Support', 'wpt_pro' ); ?></h3>
+            </a>
+            <span><?php echo esc_html__( 'Discussion:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/discussions" target="_blank"><?php echo esc_html__( 'Forum and Discussion on (Github)', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'Issue on Github:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/issues" target="_blank"><?php echo esc_html__( 'Submit your Issue on (Github)', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'Topic on WordPress.org:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/issues" target="_blank"><?php echo esc_html__( 'Submit your topic on (WordPress.org)', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'Email:', 'wpt_pro' ); ?> <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&su=<?php echo urlencode("Need help for " . site_url()); ?>&body=<?php echo urlencode("Hey Saiful, I need help.\nNow I will explain my Issue.\n...________\n"); ?>&ui=2&tf=1&to=<?php echo urlencode("Saiful Islam<codersaiful@gmail.com>"); ?>" target="_blank"><?php echo esc_html__( 'Gmail Me <b>(codersaiful@gmail.com)</b>', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'FB Group:', 'wpt_pro' ); ?> <a target="_blank" href="https://www.facebook.com/groups/CodeAstrology/"><?php echo esc_html__( 'Join to CodeAstrology Group', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'Skype:', 'wpt_pro' ); ?> <a href="#"><?php echo esc_html__( 'codersaiful', 'wpt_pro' ); ?></a></span>
+            
+            <span><?php echo esc_html__( 'Facebook:', 'wpt_pro' ); ?> <a href="https://www.facebook.com/messages/t/codersaiful" target="_blank"><?php echo esc_html__( 'Message to CodeAstorlogy', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'Facebook:', 'wpt_pro' ); ?> <a href="https://www.facebook.com/CodeAstrology" target="_blank"><?php echo esc_html__( 'CodeAstrology Page', 'wpt_pro' ); ?></a></span>
+            <hr/>
+            <span><?php echo esc_html__( 'Facebook:', 'wpt_pro' ); ?> <a href="https://www.facebook.com/messages/t/codersaiful" target="_blank"><?php echo esc_html__( 'Message to Author', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'Facebook:', 'wpt_pro' ); ?> <a href="https://www.facebook.com/codersaiful" target="_blank"><?php echo esc_html__( 'Author Profile', 'wpt_pro' ); ?></a></span>
+            
+            <span><?php echo esc_html__( 'Youtube:', 'wpt_pro' ); ?> <a href="https://www.youtube.com/playlist?list=PL__UrUIHRxdzupiUk2KKJloistwDMP28t" target="_blank"><?php echo esc_html__( 'Youtube Playlist - Video Tutorial', 'wpt_pro' ); ?></a></span>
+        </div>
+    </div>
+    
+
+    <div class="wpt_right_side_in_wrapper" style="padding: 10px;">
+        <div class="need_help_wrapper">
+            <h1><?php echo esc_html__( 'Are you Coder?', 'wpt_pro' ); ?></h1>
+            
+            <span class="fork-button-wpt"><?php echo esc_html__( 'To be a Contributor', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/fork?fragment=1" target="_blank"><?php echo esc_html__( 'Fork and (Contribute)', 'wpt_pro' ); ?></a></span>
+            
+            
+            <span><?php echo esc_html__( 'GitHub:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/discussions" target="_blank"><?php echo esc_html__( 'Woo Product Table on (Github)', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'Discussion:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/discussions" target="_blank"><?php echo esc_html__( 'Forum and Discussion on (Github)', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'Issue on Github:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/issues" target="_blank"><?php echo esc_html__( 'Submit your Issue on (Github)', 'wpt_pro' ); ?></a></span>
+            <span><?php echo esc_html__( 'Topic on WordPress.org:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/issues" target="_blank"><?php echo esc_html__( 'Submit your topic on (WordPress.org)', 'wpt_pro' ); ?></a></span>
+        </div>
+    </div>
+    <div class="wpt_right_side_in_wrapper" style="padding: 10px;">
+        <div class="rate_us_wrapper">
+            <a href="https://wordpress.org/support/plugin/woo-product-table/reviews/?filter=5#new-post" target="_blank"><img class="rate_us_image" src="<?php echo WPT_Product_Table::getPath( 'BASE_URL' ); ?>assets/images/rate_us.png"></a>
+            <h1><?php echo esc_html__( 'It can take only 2 Minutes.', 'wpt_pro' ); ?></h1>
+            <a href="https://wordpress.org/support/plugin/woo-product-table/reviews/?filter=5#new-post" target="_blank"><?php echo esc_html__( 'Rate Us on WordPress.org','wpt_pro' ); ?></a>
+        </div>
+    </div>
+
+    <div class="wpt_right_side_in_wrapper" style="padding: 10px;">
+        <div class="social_title_wrapper">
             <h2 class="social_title"><img src="<?php echo WPT_Product_Table::getPath( 'BASE_URL' ); ?>assets/images/social/social.png"> Share on Social Network</h2>
             <p><?php echo esc_html__( 'Your one share can be best GIFT for me.', 'wpt_pro' ); ?></p>
             <div class="social_links">
@@ -34,52 +99,6 @@
 
             </div>
             <small><?php echo esc_html__( 'Feel free to knock me Any time.', 'wpt_pro' ); ?></small>
-        </div>
-    </div>
-    
-    <div class="wpt_right_side_in_wrapper" style="padding: 10px;">
-        <div class="need_help_wrapper">
-            <h1><?php echo esc_html__( 'Need Help?', 'wpt_pro' ); ?></h1>
-            <a style="display: block;text-align: left;" target="_blank" href="https://codeastrology.com/support/">
-                <img style="max-width: 100px" src="<?php echo WPT_BASE_URL; ?>assets/images/customer_support.png">
-                <h3><?php echo esc_html__( 'Customer Support', 'wpt_pro' ); ?></h3>
-            </a>
-            <span><?php echo esc_html__( 'Discussion:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/discussions" target="_blank"><?php echo esc_html__( 'Forum and Discussion on (Github)', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'Issue on Github:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/issues" target="_blank"><?php echo esc_html__( 'Submit your Issue on (Github)', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'Topin on WordPress.org:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/issues" target="_blank"><?php echo esc_html__( 'Submit your topic on (WordPress.org)', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'Email:', 'wpt_pro' ); ?> <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&su=<?php echo urlencode("Need help for " . site_url()); ?>&body=<?php echo urlencode("Hey Saiful, I need help.\nNow I will explain my Issue.\n...________\n"); ?>&ui=2&tf=1&to=<?php echo urlencode("Saiful Islam<codersaiful@gmail.com>"); ?>" target="_blank"><?php echo esc_html__( 'Gmail Me <b>(codersaiful@gmail.com)</b>', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'FB Group:', 'wpt_pro' ); ?> <a target="_blank" href="https://www.facebook.com/groups/CodeAstrology/"><?php echo esc_html__( 'Join to CodeAstrology Group', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'Skype:', 'wpt_pro' ); ?> <a href="#"><?php echo esc_html__( 'codersaiful', 'wpt_pro' ); ?></a></span>
-            
-            <span><?php echo esc_html__( 'Facebook:', 'wpt_pro' ); ?> <a href="https://www.facebook.com/messages/t/codersaiful" target="_blank"><?php echo esc_html__( 'Message to CodeAstorlogy', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'Facebook:', 'wpt_pro' ); ?> <a href="https://www.facebook.com/CodeAstrology" target="_blank"><?php echo esc_html__( 'CodeAstrology Page', 'wpt_pro' ); ?></a></span>
-            <hr/>
-            <span><?php echo esc_html__( 'Facebook:', 'wpt_pro' ); ?> <a href="https://www.facebook.com/messages/t/codersaiful" target="_blank"><?php echo esc_html__( 'Message to Author', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'Facebook:', 'wpt_pro' ); ?> <a href="https://www.facebook.com/codersaiful" target="_blank"><?php echo esc_html__( 'Author Profile', 'wpt_pro' ); ?></a></span>
-            
-            <span><?php echo esc_html__( 'Youtube:', 'wpt_pro' ); ?> <a href="https://www.youtube.com/playlist?list=PL__UrUIHRxdzupiUk2KKJloistwDMP28t" target="_blank"><?php echo esc_html__( 'Youtube Playlist - Video Tutorial', 'wpt_pro' ); ?></a></span>
-        </div>
-    </div>
-    
-
-    <div class="wpt_right_side_in_wrapper" style="padding: 10px;">
-        <div class="need_help_wrapper">
-            <h1><?php echo esc_html__( 'Are you Coder?', 'wpt_pro' ); ?></h1>
-            
-            <span class="fork-button-wpt"><?php echo esc_html__( 'To be a Contributor', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/fork?fragment=1" target="_blank"><?php echo esc_html__( 'Fork and (Contribute)', 'wpt_pro' ); ?></a></span>
-            
-            
-            <span><?php echo esc_html__( 'GitHub:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/discussions" target="_blank"><?php echo esc_html__( 'Woo Product Table on (Github)', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'Discussion:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/discussions" target="_blank"><?php echo esc_html__( 'Forum and Discussion on (Github)', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'Issue on Github:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/issues" target="_blank"><?php echo esc_html__( 'Submit your Issue on (Github)', 'wpt_pro' ); ?></a></span>
-            <span><?php echo esc_html__( 'Topin on WordPress.org:', 'wpt_pro' ); ?> <a href="https://github.com/codersaiful/woo-product-table/issues" target="_blank"><?php echo esc_html__( 'Submit your topic on (WordPress.org)', 'wpt_pro' ); ?></a></span>
-        </div>
-    </div>
-    <div class="wpt_right_side_in_wrapper" style="padding: 10px;">
-        <div class="rate_us_wrapper">
-            <a href="https://wordpress.org/support/plugin/woo-product-table/reviews/?filter=5#new-post" target="_blank"><img class="rate_us_image" src="<?php echo WPT_Product_Table::getPath( 'BASE_URL' ); ?>assets/images/rate_us.png"></a>
-            <h1><?php echo esc_html__( 'It can take only 2 Minutes.', 'wpt_pro' ); ?></h1>
-            <a href="https://wordpress.org/support/plugin/woo-product-table/reviews/?filter=5#new-post" target="_blank"><?php echo esc_html__( 'Rate Us on WordPress.org','wpt_pro' ); ?></a>
         </div>
     </div>
 
