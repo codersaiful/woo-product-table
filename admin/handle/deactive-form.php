d13310ae3c42421afca18c3b6aa96be59d5b69a3
Saiful Islam <codersaiful@gmail.com>
2022-10-08 16:46:07 +0600

    still working

--- admin/handle/deactive-form.php
+++ admin/handle/deactive-form.php
@@ -11,18 +11,57 @@ class Deactive_Form extends Base
     protected $assignScreen = false;
     protected $screen;
     protected $screenID;
-    protected $data = [
-        'plugin-slug' => 'woo-product-table',
-        'prefix' => 'wpt',
-    ];
+    protected $plugin_slug = 'woo-product-table';
+    protected $prefix = 'wpt';
 
+    /**
+     * Change also it to message.js file.
+     * Otherwise, It will not work.
+     *
+     * @var string
+     */
+    protected $localize_name = 'WPT_DEACTIVE_DATA';
+    
+
+    /**
+     * Verymuch Important. It's screen name. It will be always plugins obviously.
+     *
+     * @var string
+     */
     protected $required_screen_id = 'edit-wpt_product_table'; // 'plugins';
+
+    protected $support_url = 'https://codeastrology.com/support/submit-ticket/';
+
     public function run()
     {
+        /**
+         * If you don't habe Base Class where available assets_url,
+         * Fix at bellow.
+         */
+        $this->js_file = $this->assets_url . 'js/message.js';
+        $this->css_file = $this->assets_url . 'css/message.css';
+
+
         add_action('admin_footer', array($this, 'form'));
         add_action('admin_enqueue_scripts', [$this, 'enqueue']);
     }
 
+    /**
+     * Display Main form, When user will try to Deactivate Plugin
+     * 
+     * SOME CLUE
+     * -----------
+     * <div id="<?php echo esc_attr( $this->prefix ); ?>-survey-form-wrap">
+     * Why I used prefix at the id name.
+     * ********************************
+     * Actually we have to use this class different plugin, when every plugin's element should different,
+     * so to define different element of different plugin,
+     * I have used prefix actually.
+     * 
+     * @author Saiful Islam <codersaiful@gmail.com>
+     *
+     * @return void
+     */
     public function form()
     {
         if (!$this->assignScreen) $this->assignScreen();
@@ -32,8 +71,8 @@ class Deactive_Form extends Base
         $site_url = get_site_url();
         $blog_name = get_bloginfo( 'name' );
 ?>
-        <div id="wpt-survey-form-wrap">
-            <div id="wpt-survey-form">
+        <div id="<?php echo esc_attr( $this->prefix ); ?>-survey-form-wrap" class="ca-survey-form-wrap">
+            <div id="<?php echo esc_attr( $this->prefix ); ?>-survey-form" class="ca-survey-form">
                 <p class="motivational-speek">
                     If you have a moment, please let us know why you are deactivating.
                      <!-- All submissions are anonymous and we only use this feedback for improving our plugin. -->
@@ -44,57 +83,57 @@ class Deactive_Form extends Base
                     <input name="Date" type="hidden" placeholder="Date" value="<?php echo esc_attr( $date ); ?>" required>
                     <input name="Website" type="hidden" placeholder="Website" value="<?php echo esc_attr( $site_url ); ?>" required>
                     <input name="Title" type="hidden" placeholder="Title" value="<?php echo esc_attr( $blog_name ); ?>" required>
-                    <div class="wpt-msg-field-wrapper">
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_temporarily" name="Reason" value="I'm only deactivating temporarily">
-                            <label for="wpt_temporarily">I'm only deactivating temporarily</label>
+                    <div class="ca-msg-field-wrapper">
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_temporarily" name="Reason" value="I'm only deactivating temporarily">
+                            <label for="ca_temporarily">I'm only deactivating temporarily</label>
                         </div>
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_notneeded" name="Reason" value="I no longer need the plugin">
-                            <label for="wpt_notneeded">I no longer need the plugin</label>
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_notneeded" name="Reason" value="I no longer need the plugin">
+                            <label for="ca_notneeded">I no longer need the plugin</label>
                         </div>
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_short" name="Reason" value="I only needed the plugin for a short period">
-                            <label for="wpt_short">I only needed the plugin for a short period</label>
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_short" name="Reason" value="I only needed the plugin for a short period">
+                            <label for="ca_short">I only needed the plugin for a short period</label>
                         </div>
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_better" name="Reason" value="I found a better plugin">
-                            <label for="wpt_better">I found a better plugin</label>
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_better" name="Reason" value="I found a better plugin">
+                            <label for="ca_better">I found a better plugin</label>
                         </div>
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_upgrade" name="Reason" value="Upgrading to PRO version">
-                            <label for="wpt_upgrade">Upgrading to PRO version</label>
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_upgrade" name="Reason" value="Upgrading to PRO version">
+                            <label for="ca_upgrade">Upgrading to PRO version</label>
                         </div>
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_requirement" name="Reason" value="Plugin doesn\'t meets my requirement">
-                            <label for="wpt_requirement">Plugin doesn't meets my requirement</label>
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_requirement" name="Reason" value="Plugin doesn\'t meets my requirement">
+                            <label for="ca_requirement">Plugin doesn't meets my requirement</label>
                         </div>
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_broke" name="Reason" value="Plugin broke my site">
-                            <label for="wpt_broke">Plugin broke my site</label>
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_broke" name="Reason" value="Plugin broke my site">
+                            <label for="ca_broke">Plugin broke my site</label>
                         </div>
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_stopped" name="Reason" value="Plugin suddenly stopped working">
-                            <label for="wpt_stopped">Plugin suddenly stopped working</label>
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_stopped" name="Reason" value="Plugin suddenly stopped working">
+                            <label for="ca_stopped">Plugin suddenly stopped working</label>
                         </div>
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_bug" name="Reason" value="I found a bug">
-                            <label for="wpt_bug">I found a bug</label>
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_bug" name="Reason" value="I found a bug">
+                            <label for="ca_bug">I found a bug</label>
                         </div>
-                        <div class="wpt-each-field">
-                            <input type="radio" id="wpt_other" name="Reason" value="Other">
-                            <label for="wpt_other">Other</label>
+                        <div class="ca-each-field">
+                            <input type="radio" id="ca_other" name="Reason" value="Other">
+                            <label for="ca_other">Other</label>
                         </div>
                     </div>
-                    <p id="wpt-error"></p>
-                    <div class="wpt-comments" style="display:nones;">
+                    <p id="ca-error"></p>
+                    <div class="ca-comments" style="display:nones;">
                         <textarea type="text" name="Comments" placeholder="Please describe (If possible)" rows="2"></textarea>
-                        <p>For support queries <a href="https://codeastrology.com/support/submit-ticket/" target="_blank">Submit Ticket</a></p>
+                        <p>For support queries <a href="<?php echo esc_url( $this->support_url ); ?>" target="_blank">Submit Ticket</a></p>
                     </div>
-                    <div class="wpt-msg-button-wrapper">
-                        <button type="submit" class="wpt_button wpt-deactive" id="wpt_deactivate">Submit & Deactivate</button>
-                        <a href="#" class="wpt_button" id="wpt_cancel">Keep</a>
-                        <a href="#" class="wpt_button" id="wpt_skip">Skip & Deactivate</a>
+                    <div class="ca-msg-button-wrapper">
+                        <button type="submit" class="ca_button ca-deactive" id="ca_deactivate">Submit & Deactivate</button>
+                        <a href="#" class="ca_button" id="ca_cancel">Keep</a>
+                        <a href="#" class="ca_button" id="ca_skip">Skip & Deactivate</a>
                     </div>
                 </form>
             </div>
@@ -110,21 +149,26 @@ class Deactive_Form extends Base
         if (!$this->assignScreen) $this->assignScreen();
         if ($this->screenID !== $this->required_screen_id) return;
 
-        $this->js_file = $this->assets_url . 'js/message.js';
-        $this->css_file = $this->assets_url . 'css/message.css';
-
         wp_enqueue_script('jquery');
+        $enq_name = $this->plugin_slug . '-msg';
+        
 
-        $WPT_DATA = array( 
-            'ajaxurl' => 'sss',
- 
-            );
-        wp_localize_script( 'wpt-deactive-message', 'WPT_DATA_ADMIN', $WPT_DATA );
-
-        wp_enqueue_script('wpt-deactive-message', $this->js_file, array('jquery'), $this->dev_version, true);
-        wp_enqueue_style( 'wpt-deactive-message', $this->css_file, array('wpt-admin'), $this->dev_version, 'all' );
+        wp_enqueue_script( $enq_name, $this->js_file, array('jquery'), $this->dev_version, true);
+        $data = [
+            'plugin_slug' => $this->plugin_slug,
+            'prefix' => $this->prefix,
+        ];
+        
+        wp_localize_script( $enq_name, $this->localize_name, $data );
+        wp_enqueue_style( $enq_name, $this->css_file, array('wpt-admin'), $this->dev_version, 'all' );
     }
 
+    /**
+     * I have assignScreen Once time only
+     * And by this, I able to check Screen.
+     *
+     * @return void
+     */
     private function assignScreen()
     {
         if (!function_exists('get_current_screen')) return;
--- assets/js/message.js
+++ assets/js/message.js
@@ -1,7 +1,23 @@
 jQuery(function($) {
     'use strict';
     $(document).ready(function() {
-        console.log("Something");
+        var base_data = WPT_DEACTIVE_DATA;
+
+
+
+        let prefix = base_data.prefix;
+        let wrapper_id = '#' + prefix + '-survey-form-wrap';
+        let deactive_btn_id = '#deactivate-' + base_data.plugin_slug;
+
+        var wrapperElement = $(wrapper_id);
+        var FormElementWrapper = wrapperElement.find('ca-survey-form');
+        var deactiveBtn = $(deactive_btn_id);
+
+        $(document.body).on('click',function(){
+            
+        });
+
+        console.log(base_data,"Something");
         
     });
 });
