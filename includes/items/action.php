c16dfa079979cab655e64900b0e21eb0e2412259
Saiful Islam <codersaiful@gmail.com>
2022-10-12 17:42:29 +0600

    for non ajax - going new tab issue has been fixed

--- includes/items/action.php
+++ includes/items/action.php
@@ -47,7 +47,7 @@ if( $table_type == 'advance_table'){
     $target = '_self';
     if( $ajax_action !== 'ajax_active ' ){
         $add_to_cart_url = get_the_permalink( $id ) . '?from=wpt';
-        $target = '_blank';
+        // $target = '_blank';
     }
     /*else{
         $add_to_cart_url =  $product->add_to_cart_url();//'?add-to-cart=' .  $data['id'];//( $ajax_action == 'no_ajax_action' ? '?add-to-cart=' .  $data['id'] : '?add-to-cart=' .  $data['id'] );// '?add-to-cart=' .  $data['id'];
