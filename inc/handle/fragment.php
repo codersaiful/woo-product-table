d2e75979fc4a918a9e373f7329f79c6866bed85f
Saiful Islam <codersaiful@gmail.com>
2022-10-20 09:36:08 +0600

    wpt_pro textdomain fixed on fragments

--- inc/handle/fragment.php
+++ inc/handle/fragment.php
@@ -141,15 +141,15 @@ if( $this->cart_lists && $this->cart_stats ){
 <div class="wpt-cart-contents">
     <?php echo wp_kses_post( WC()->cart->get_cart_subtotal() ); ?> 
     <span class="count">
-        <?php echo wp_kses_data( sprintf( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count(), 'wpt-pro' ), WC()->cart->get_cart_contents_count() ) ); ?>
+        <?php echo wp_kses_data( sprintf( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count(), 'wpt_pro' ), WC()->cart->get_cart_contents_count() ) ); ?>
     </span>
 <?php if( $this->cart_stats ){ ?>
-    <span title="<?php echo esc_attr__( 'Empty Cart.', 'wpt-pro' ); ?>" class="wpt_empty_cart_btn">
+    <span title="<?php echo esc_attr__( 'Empty Cart.', 'wpt_pro' ); ?>" class="wpt_empty_cart_btn">
         <i class="wpt-trash-empty"></i>
     </span>
 <?php } ?>
 </div>
-<a target="_blank" href="<?php echo esc_url( wc_get_cart_url() ); ?>" class="wpt-view-n"><?php echo esc_html__( 'View cart', 'wpt-pro' ); ?> <i class="wpt-bag"></i></a>
+<a target="_blank" href="<?php echo esc_url( wc_get_cart_url() ); ?>" class="wpt-view-n"><?php echo esc_html__( 'View cart', 'wpt_pro' ); ?> <i class="wpt-bag"></i></a>
 
 </div>
 </div>
