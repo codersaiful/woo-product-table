eae05e233610083798fedc3b16926ed1d8989fe2
Saiful Islam <codersaiful@gmail.com>
2022-11-03 12:23:03 +0600

    function transfer row to base table

--- inc/table/row.php
+++ inc/table/row.php
@@ -175,24 +175,7 @@ class Row extends Table_Base{
 
     }
 
-    /**
-     * String with comma to Array, 
-     * I will use explode function and will convert to Array.
-     * 
-     * ******
-     * Specially maked for Old user, who has created his table many time ago.
-     *
-     * @param string $string_txt Required string but if anyone set any other content, it will fix.
-     * @return array
-     */
-    protected function string_to_array( $string_txt ){
-        if( empty( $string_txt ) ) return [];
-        if( is_array( $string_txt ) ) return $string_txt;
-        if( ! is_string( $string_txt ) ) return [];
-        $string_txt = rtrim( $string_txt, ', ' );
-        $string_txt = ltrim( $string_txt, ', ' );
-        return explode( ',', $string_txt );
-    }
+    
     public function render(){
         global $product;
         if($this->wp_force){
