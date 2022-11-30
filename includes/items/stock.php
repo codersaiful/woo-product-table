82861ddb27bcde83f35fa7fa431afbbdf4fa18a6
Fazle Bari <fazlebarisn@gmail.com>
2022-11-30 11:05:09 +0600

    display stock status

--- includes/items/stock.php
+++ includes/items/stock.php
@@ -1,20 +1,10 @@
 <?php
-echo wc_get_stock_html( $product );
-/*
-$stock_status_message = $stock_status_message = $config_value['table_out_of_stock'];
-if( $data['stock_status'] == 'instock' ){
-    $stock_status_message =  $data['stock_quantity'] . ' ' . $config_value['table_in_stock']; 
-}elseif( $data['stock_status'] == 'onbackorder' ){
-    $stock_status_message = $config_value['table_on_back_order'];//'On Back Order';
-}
- * //stock old style change and added wc_get_sthock
-echo "<span class='{$data['stock_status']}'>" . $stock_status_message . " </span>";
-*/
 
-// 
 $data= $product->get_data();
 $stock = $data['stock_status'];
 
 if ( ! $product->managing_stock() && 'instock' == $stock){
-    echo "<p>In Stock</p>";
+    echo "<p class='wpt_instock'>In Stock</p>";
+}else{
+    echo wc_get_stock_html( $product );
 }
