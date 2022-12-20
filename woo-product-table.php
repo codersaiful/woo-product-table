ff397f180a7efeb264e82b12b7b6c90179064aaa
Saiful Islam <codersaiful@gmail.com>
2022-12-20 17:04:49 +0600

    wc upto tested added

--- woo-product-table.php
+++ woo-product-table.php
@@ -11,7 +11,7 @@
  * Requires at least:    5.0.0
  * Tested up to:         6.1.1
  * WC requires at least: 5.0.0
- * WC tested up to: 	 7.2.0
+ * WC tested up to: 	 7.2.1
  * 
  * 
  * Text Domain: woo-product-table
