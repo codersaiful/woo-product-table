6fbb0be5170818a19c6672d9bd9820e89df80de2
Saiful Islam <codersaiful@gmail.com>
2023-03-13 18:07:05 +0600

    tested upto 6.2

--- woo-product-table.php
+++ woo-product-table.php
@@ -9,7 +9,7 @@
  * 
  * Version: 3.3.8
  * Requires at least:    5.0.0
- * Tested up to:         6.1.1
+ * Tested up to:         6.2
  * WC requires at least: 5.0.0
  * WC tested up to: 	 7.4.1
  * 
