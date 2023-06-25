52a470eb84151ac286c868f408470da9edf3b382
Fazle Bari <fazlebarisn@gmail.com>
2023-06-25 11:21:04 +0600

    set stock quantity as the max quantity

--- includes/items/quantity.php
+++ includes/items/quantity.php
@@ -14,7 +14,7 @@ if( $product->is_sold_individually() ) return false;
 
 $t_args = array( 
     'input_value'   => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
-    'max_value'   => apply_filters( 'woocommerce_quantity_input_max', -1, $product ),
+    'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_stock_quantity(), $product ),
     'min_value'   => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
     'step'        => apply_filters( 'woocommerce_quantity_input_step', 1, $product ),
 );
