7f85e66c09595c8a0d951d58fe1c631de12d9d95
Saiful Islam <codersaiful@gmail.com>
2023-08-31 17:01:43 +0600

    working

--- gutt/js/wpt-block.js
+++ gutt/js/wpt-block.js
@@ -1,55 +1,33 @@
 console.log('HHHHHHHHHHHHHHHHHHHHHHHHHHHH');
 console.log(wp);
 (function (blocks, editor, components, i18n, element) {
+    console.log(blocks, editor, components, i18n, element);
     var el = element.createElement;
     var TextControl = components.TextControl;
-    var SelectControl = components.SelectControl;
-    var apiFetch = wp.apiFetch;
 
     blocks.registerBlockType('woo-product-table/wpt-block', {
-        title: i18n.__('Woo Product Table Block', 'woo-product-table'),
+        title: i18n.__('Woo Product Table', 'woo-product-table'),
         icon: 'smiley',
         category: 'common',
         attributes: {
             productId: {
-                type: 'string',
-                default: '', // Default dynamic parameter
-            },
-            productOptions: {
-                type: 'array',
-                default: [],
+                type: 'int',
+                default: '123', // Default dynamic parameter
             },
         },
         edit: function (props) {
             var productId = props.attributes.productId;
-            var productOptions = props.attributes.productOptions;
 
             function onChangeProductId(newProductId) {
                 props.setAttributes({ productId: newProductId });
             }
 
-            function fetchProductOptions() {
-                apiFetch({
-                    url: '/wp/v2/wpt_product_table', // Adjust the REST API endpoint
-                }).then(function (products) {
-                    var options = products.map(function (product) {
-                        return { value: product.id, label: product.title.rendered };
-                    });
-                    props.setAttributes({ productOptions: options });
-                });
-            }
-
-            if (productOptions.length === 0) {
-                fetchProductOptions();
-            }
-
             return el(
                 'div',
                 { className: 'wpt-block-wrapper' },
-                el(SelectControl, {
-                    label: i18n.__('Select Product', 'woo-product-table'),
+                el(TextControl, {
+                    label: i18n.__('Product ID', 'your-plugin'),
                     value: productId,
-                    options: productOptions,
                     onChange: onChangeProductId,
                 }),
                 el('p', {
@@ -58,7 +36,12 @@ console.log(wp);
                 })
             );
         },
-        // ...
+        save: function (props) {
+            return el('div', {
+                className: 'wpt-block-wrapper',
+                dangerouslySetInnerHTML: { __html: `[Product_Table id='${props.attributes.productId}']` },
+            });
+        },
     });
 })(
     window.wp.blocks,
