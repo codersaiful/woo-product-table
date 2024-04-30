4f28e84e802013156a32c5803257dfd90523d924
Saiful Islam <codersaiful@gmail.com>
2024-04-30 12:19:20 +0600

    wc and wp updated updated

--- woo-product-table.php
+++ woo-product-table.php
@@ -11,7 +11,7 @@
  * Requires at least:    4.0.0
  * Tested up to:         6.5.2
  * WC requires at least: 5.0.0
- * WC tested up to: 	 8.7.2
+ * WC tested up to: 	 8.8.3
  * 
  * 
  * Text Domain: woo-product-table
