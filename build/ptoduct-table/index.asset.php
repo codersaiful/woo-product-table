cd3ae98288c0589760c1cc4e42c8da18cc9cb10b
Saiful Islam <codersaiful@gmail.com>
2025-06-27 12:48:14 +0600

    simple block added

---
+++ build/ptoduct-table/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a9dd894e6377dd0534e4');
